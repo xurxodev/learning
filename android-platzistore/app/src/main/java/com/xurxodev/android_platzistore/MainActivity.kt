9e7232fe51a46e8b57478ce0922c1c4d921fd2f4
Jorge Sánchez <xurxosf@gmail.com>
2018-08-24 11:48:46 +0200

    android-plaztistore - add Anko library

--- android-platzistore/app/src/main/java/com/xurxodev/android_platzistore/MainActivity.kt
+++ android-platzistore/app/src/main/java/com/xurxodev/android_platzistore/MainActivity.kt
@@ -2,11 +2,14 @@ package com.xurxodev.android_platzistore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast("Toast from Anko")
     }
 }
